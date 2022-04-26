33ddfffdf5dd261db874e68dabc05dd380637d53
David Lusher <D.Lusher@soton.ac.uk>
2022-04-26 16:53:40 +0900

    NACA4412 multi-block problem

--- apps/generate_all_applications.py
+++ apps/generate_all_applications.py
@@ -4,7 +4,6 @@ import os, subprocess
 directories = [\
 '/wave/',
 '/euler_wave/',
-'/euler_wave_curvilinear/',
 '/shu_osher/',
 '/Sod_shock_tube/',
 '/taylor_green_vortex/',
@@ -18,11 +17,14 @@ directories = [\
 '/channel_flow/turbulent_3D/',
 '/channel_flow/compressible_TCF_Central/',
 '/channel_flow/compressible_TCF_TENO/',
+'/Delery_bump/inviscid/',
+'/Delery_bump/viscous/',
 '/transitional_SBLI/',
+'/cylinder/supersonic_cylinder/',
+'/aerofoils/multi_block/NACA4412/3D/'
 ]
 file_names = [\
 'wave.py',
-'euler_wave.py',
 'euler_wave.py',
 'shu_osher.py',
 'Sod_shock_tube.py',
@@ -37,7 +39,11 @@ file_names = [\
 'turbulent_channel.py',
 'turbulent_channel.py',
 'turbulent_channel.py',
+'inviscid_shock_delery_aerofoil_forced.py',
+'viscous_shock_delery_aerofoil.py',
 'transitional_SBLI.py',
+'supersonic_cylinder.py',
+'NACA4412.py'
 ]
 
 assert len(directories) == len(file_names)
