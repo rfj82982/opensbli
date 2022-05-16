a02c4c82f0051443f9f29a4344e2ae14964baf99
David <d.lusher@soton.ac.uk>
2022-05-16 15:58:38 +0900

    Fixed multi-block TGV initial condition

--- apps/taylor_green_vortex/TGV_multi_block/taylor_green_vortex.py
+++ apps/taylor_green_vortex/TGV_multi_block/taylor_green_vortex.py
@@ -5,6 +5,49 @@ from opensbli import *
 import copy
 from opensbli.utilities.helperfunctions import substitute_simulation_parameters
 
+def TGV_initial_condition(block_number):
+    # Create a simulation block
+    block = SimulationBlock(ndim, block_number=block_number)
+    # Local dictionary for parsing the expressions
+    local_dict = {"block": block, "GridVariable": GridVariable, "DataObject": DataObject}
+    # Set the local coordinates
+    if block_number == 0:
+        x0 = "Eq(DataObject(x0), block.deltas[0]*block.grid_indexes[0])"
+        x1 = "Eq(DataObject(x1), block.deltas[1]*block.grid_indexes[1])"
+    elif block_number == 1 :
+        x0 = "Eq(DataObject(x0), M_PI + block.deltas[0]*block.grid_indexes[0])"
+        x1 = "Eq(DataObject(x1), block.deltas[1]*block.grid_indexes[1])"
+    elif block_number == 2:
+        x0 = "Eq(DataObject(x0), block.deltas[0]*block.grid_indexes[0])"
+        x1 = "Eq(DataObject(x1), M_PI + block.deltas[1]*block.grid_indexes[1])"
+    elif block_number == 3:
+        x0 = "Eq(DataObject(x0), M_PI + block.deltas[0]*block.grid_indexes[0])"
+        x1 = "Eq(DataObject(x1), M_PI + block.deltas[1]*block.grid_indexes[1])"
+    else:
+        raise NotImplementedError("The multi-block case has been defined for 4 blocks.")
+    # z coordinate
+    x2 = "Eq(DataObject(x2), block.deltas[2]*block.grid_indexes[2])"       
+    coords = [parse_expr(eq, local_dict=local_dict) for eq in [x0, x1, x2]]
+
+    # Initial conditions as strings
+    u0 = "Eq(GridVariable(u0),sin(DataObject(x0))*cos(DataObject(x1))*cos(DataObject(x2)))"
+    u1 = "Eq(GridVariable(u1),-cos(DataObject(x0))*sin(DataObject(x1))*cos(DataObject(x2)))"
+    u2 = "Eq(GridVariable(u2), 0.0)"
+    p = "Eq(GridVariable(p), 1.0/(gama*Minf*Minf)+ (1.0/16.0) * (cos(2.0*DataObject(x0))+cos(2.0*DataObject(x1)))*(2.0 + cos(2.0*DataObject(x2))))"
+    r = "Eq(GridVariable(r), gama*Minf*Minf*p)"
+    # Conservative form
+    rho = "Eq(DataObject(rho), r)"
+    rhou0 = "Eq(DataObject(rhou0), r*u0)"
+    rhou1 = "Eq(DataObject(rhou1), r*u1)"
+    rhou2 = "Eq(DataObject(rhou2), r*u2)"
+    rhoE = "Eq(DataObject(rhoE), p/(gama-1) + 0.5* r *(u0**2+ u1**2 + u2**2))"
+    # Parse the initial conditions
+    vortex_condition = [parse_expr(eq, local_dict=local_dict) for eq in [u0, u1, u2, p, r, rho, rhou0, rhou1, rhou2, rhoE]]
+    
+    init_class = GridBasedInitialisation()
+    init_class.add_equations(coords + vortex_condition)
+    return [init_class]
+
 # Number of dimensions of the system to be solved
 ndim = 3
 nblocks = 4
@@ -80,76 +123,12 @@ for index, eq in enumerate(flatten(constituent.equations)):
 
 latex.close()
 
-# Create a simulation block
-block = SimulationBlock(ndim, block_number=0)
-
-# Local dictionary for parsing the expressions
-local_dict = {"block": block, "GridVariable": GridVariable, "DataObject": DataObject}
-
-# Initial conditions as strings
-x0 = "Eq(GridVariable(x0), block.deltas[0]*block.grid_indexes[0])"
-x1 = "Eq(GridVariable(x1), block.deltas[1]*block.grid_indexes[1])"
-x2 = "Eq(GridVariable(x2), block.deltas[2]*block.grid_indexes[2])"
-
-u0 = "Eq(GridVariable(u0),sin(DataObject(x0))*cos(DataObject(x1))*cos(DataObject(x2)))"
-u1 = "Eq(GridVariable(u1),-cos(DataObject(x0))*sin(DataObject(x1))*cos(DataObject(x2)))"
-u2 = "Eq(GridVariable(u2), 0.0)"
-p = "Eq(GridVariable(p), 1.0/(gama*Minf*Minf)+ (1.0/16.0) * (cos(2.0*DataObject(x0))+cos(2.0*DataObject(x1)))*(2.0 + cos(2.0*DataObject(x2))))"
-r = "Eq(GridVariable(r), gama*Minf*Minf*p)"
-
-rho = "Eq(DataObject(rho), r)"
-rhou0 = "Eq(DataObject(rhou0), r*u0)"
-rhou1 = "Eq(DataObject(rhou1), r*u1)"
-rhou2 = "Eq(DataObject(rhou2), r*u2)"
-rhoE = "Eq(DataObject(rhoE), p/(gama-1) + 0.5* r *(u0**2+ u1**2 + u2**2))"
-
-eqns = [u0, u1, u2, p, r, rho, rhou0, rhou1, rhou2, rhoE]
-
-# parse the initial conditions
-initial_equations = [parse_expr(eq, local_dict=local_dict) for eq in eqns]
-initial = GridBasedInitialisation()
-initial.add_equations(initial_equations)
-
+# Set the initial conditions on each of the blocks
 mb_initial_conditions = {0:None, 1:None, 2:None, 3:None}
-# Block 0
-block0_init = []
-x0 = "Eq(DataObject(x0), block.deltas[0]*block.grid_indexes[0])"
-x1 = "Eq(DataObject(x1), block.deltas[1]*block.grid_indexes[1])"
-x2 = "Eq(DataObject(x2), block.deltas[2]*block.grid_indexes[2])"
-coords = [parse_expr(eq, local_dict=local_dict) for eq in [x0, x1, x2]]
-init_coords = GridBasedInitialisation()
-init_coords.add_equations(coords + initial_equations)
-mb_initial_conditions[0] = [init_coords]
-
-# Block 1
-block1_init = []
-x0 = "Eq(DataObject(x0), M_PI + block.deltas[0]*block.grid_indexes[0])"
-x1 = "Eq(DataObject(x1), block.deltas[1]*block.grid_indexes[1])"
-x2 = "Eq(DataObject(x2), block.deltas[2]*block.grid_indexes[2])"
-coords = [parse_expr(eq, local_dict=local_dict) for eq in [x0, x1, x2]]
-init_coords = GridBasedInitialisation()
-init_coords.add_equations(coords + initial_equations)
-mb_initial_conditions[1] = [init_coords]
-
-# Block 2
-block2_init = []
-x0 = "Eq(DataObject(x0), block.deltas[0]*block.grid_indexes[0])"
-x1 = "Eq(DataObject(x1), M_PI + block.deltas[1]*block.grid_indexes[1])"
-x2 = "Eq(DataObject(x2), block.deltas[2]*block.grid_indexes[2])"
-coords = [parse_expr(eq, local_dict=local_dict) for eq in [x0, x1, x2]]
-init_coords = GridBasedInitialisation()
-init_coords.add_equations(coords + initial_equations)
-mb_initial_conditions[2] = [init_coords]
-
-# Block 3
-block3_init = []
-x0 = "Eq(DataObject(x0), M_PI + block.deltas[0]*block.grid_indexes[0])"
-x1 = "Eq(DataObject(x1), M_PI + block.deltas[1]*block.grid_indexes[1])"
-x2 = "Eq(DataObject(x2), block.deltas[2]*block.grid_indexes[2])"
-coords = [parse_expr(eq, local_dict=local_dict) for eq in [x0, x1, x2]]
-init_coords = GridBasedInitialisation()
-init_coords.add_equations(coords + initial_equations)
-mb_initial_conditions[3] = [init_coords]
+for i in range(nblocks):
+    init_eq = TGV_initial_condition(i)
+    mb_initial_conditions[i] = init_eq
+multi_block.set_initial_conditions(mb_initial_conditions)
 
 # Create a schemes dictionary to be used for discretisation
 schemes = {}
@@ -162,8 +141,6 @@ rk = RungeKuttaLS(3)
 schemes[rk.name] = rk
 
 # Create boundaries, one for each side per dimension, so in total 6 BC's for 3D'
-multi_block.set_initial_conditions(mb_initial_conditions)
-
 # block 0 boundary conditions
 mb_bcs = {0:None, 1:None, 2:None, 3:None}
 # Boundary conditions for block 0
