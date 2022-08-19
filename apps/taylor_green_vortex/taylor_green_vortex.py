bda65d7f53df05a26ba6c1228b5d0649a2eb4334
David Lusher <lusher.david@jaxa.jp>
2022-08-19 19:13:09 +0900

    Example of writing constants to HDF5

--- apps/taylor_green_vortex/taylor_green_vortex.py
+++ apps/taylor_green_vortex/taylor_green_vortex.py
@@ -120,6 +120,7 @@ block.set_block_boundaries(boundaries)
 kwargs = {'iotype': "Write"}
 h5 = iohdf5(save_every=10000, **kwargs)
 h5.add_arrays(simulation_eq.time_advance_arrays)
+h5.set_output_constants(['Minf', 'Re', 'Pr', 'gama'])
 # h5.add_arrays([DataObject('x0'), DataObject('x1'), DataObject('x2')])
 block.setio(copy.deepcopy(h5))
 # set the equations to be solved on the block
