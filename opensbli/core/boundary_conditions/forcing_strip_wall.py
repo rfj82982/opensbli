0c473be56446d284b0a1ced44b6e0d74be57c386
David Lusher <lusher.david@jaxa.jp>
2022-12-19 23:57:48 +0900

    Updated forced tripping wall

--- opensbli/core/boundary_conditions/forcing_strip_wall.py
+++ opensbli/core/boundary_conditions/forcing_strip_wall.py
@@ -20,10 +20,12 @@ class ForcingStripBC(ModifyCentralDerivative, BoundaryConditionBase, WallBC):
     :arg object scheme: Boundary scheme if required, defaults to Carpenter boundary treatment.
     :arg bool plane: True/False: Apply boundary condition to full range/split range only."""
 
-    def __init__(self, direction, side, wall_normal_velocity, equations, scheme=None, plane=True):
+    def __init__(self, direction, side, wall_normal_velocity, equations, corners=True, scheme=None, multi_block=False, plane=True):
         BoundaryConditionBase.__init__(self, direction, side, plane)
         self.bc_name = 'ForcingStripWall'
         self.equations = equations
+        self.corners = corners
+        self.multi_block = multi_block
         self.wall_normal_velocity = wall_normal_velocity
         if not scheme:
             self.modification_scheme = Carpenter()
@@ -32,7 +34,7 @@ class ForcingStripBC(ModifyCentralDerivative, BoundaryConditionBase, WallBC):
         return
 
     def apply(self, arrays, block):
-        halos, kernel = self.generate_boundary_kernel(block, self.bc_name)
+        halos, kernel = self.generate_boundary_kernel(block, self.bc_name, corners=self.corners)
         n_halos = abs(halos[self.direction][self.side])
         from_side_factor, to_side_factor = self.set_side_factor()
         # Using Navier Stokes physics object, create conservative variables
@@ -45,46 +47,46 @@ class ForcingStripBC(ModifyCentralDerivative, BoundaryConditionBase, WallBC):
         kernel.add_equation(wall_eqns)
 
         # Update halos if a shock capturing scheme is being used.
-        if any(isinstance(sc, ShockCapturing) for sc in block.discretisation_schemes.values()):
-            pw, gamma, Minf, Twall = GridVariable('Pwall'), NS.specific_heat_ratio(), NS.mach_number(), ConstantObject('Twall')
-            # Wall pressure
-            wall_eqns = [OpenSBLIEq(pw, increment_dataset(NS.pressure(relation=True, conservative=True), self.direction, 0))]
-            kernel.add_equation(wall_eqns)
-            # Direct copy indices over the wall
-            transfer_indices = [tuple([from_side_factor*t, to_side_factor*t]) for t in range(1, n_halos + 1)]
-            # Evaluate velocities in the halos
-            halo_velocities = []
-            for index, momentum_comp in enumerate(NS.momentum()):
-                for pair_index, index_pair in enumerate(transfer_indices):
-                    momentum, density = increment_dataset(momentum_comp, self.direction, index_pair[1]), increment_dataset(NS.density(), self.direction, index_pair[1])
-                    halo_velocities += [OpenSBLIEq(GridVariable('u%d%d' % (index, pair_index+1)), momentum/density)]
-            kernel.add_equation(halo_velocities)
-            # Evaluate the temperature one point above the wall, use it to interpolate temperature in the halos
-            T_above = GridVariable('T_above')
-            kernel.add_equation(OpenSBLIEq(GridVariable('T_above'), increment_dataset(NS.temperature(relation=True, conservative=True), self.direction, to_side_factor)))
-            halo_densities = []
-            halo_momentum = []
-            halo_energy = []
-            # Evaluate halo density using wall pressure and interpolated temperatures
+        # if any(isinstance(sc, ShockCapturing) for sc in block.discretisation_schemes.values()):
+        pw, gamma, Minf, Twall = GridVariable('Pwall'), NS.specific_heat_ratio(), NS.mach_number(), ConstantObject('Twall')
+        # Wall pressure
+        wall_eqns = [OpenSBLIEq(pw, increment_dataset(NS.pressure(relation=True, conservative=True), self.direction, 0))]
+        kernel.add_equation(wall_eqns)
+        # Direct copy indices over the wall
+        transfer_indices = [tuple([from_side_factor*t, to_side_factor*t]) for t in range(1, n_halos + 1)]
+        # Evaluate velocities in the halos
+        halo_velocities = []
+        for index, momentum_comp in enumerate(NS.momentum()):
+            for pair_index, index_pair in enumerate(transfer_indices):
+                momentum, density = increment_dataset(momentum_comp, self.direction, index_pair[1]), increment_dataset(NS.density(), self.direction, index_pair[1])
+                halo_velocities += [OpenSBLIEq(GridVariable('u%d%d' % (index, pair_index+1)), momentum/density)]
+        kernel.add_equation(halo_velocities)
+        # Evaluate the temperature one point above the wall, use it to interpolate temperature in the halos
+        T_above = GridVariable('T_above')
+        kernel.add_equation(OpenSBLIEq(GridVariable('T_above'), increment_dataset(NS.temperature(relation=True, conservative=True), self.direction, to_side_factor)))
+        halo_densities = []
+        halo_momentum = []
+        halo_energy = []
+        # Evaluate halo density using wall pressure and interpolated temperatures
 
-            for i in range(1, n_halos+1):
-                # Interpolate temperature into the halos and calculate halo density
-                rho_halo = GridVariable('rho_halo_%d' % i)
-                T_halo = GridVariable('T%d' % i)
-                kernel.add_equation([OpenSBLIEq(T_halo, (i+1)*Twall - i*T_above)])
-                halo_densities += [OpenSBLIEq(rho_halo, pw*gamma*Minf*Minf/T_halo)]
-                # Set momentum in the halos, reverse direction
-                velocities = []
-                for index, momentum_comp in enumerate(NS.momentum()):
-                    velocity = GridVariable('u%d%d' % (index, i))
-                    halo_momentum += [OpenSBLIEq(increment_dataset(momentum_comp, self.direction, from_side_factor*i), -rho_halo*velocity)]
-                    velocities += [velocity**2]
-                # Set energy in halos based on wall pressure, interpolated temperature and the calculated density
-                energy_rhs = pw/(gamma-1) + Rational(1, 2)*rho_halo*(sum(velocities))
-                halo_energy += [OpenSBLIEq(increment_dataset(NS.total_energy(), self.direction, from_side_factor*i), energy_rhs)]
-                halo_densities += [OpenSBLIEq(increment_dataset(NS.density(), self.direction, from_side_factor*i), rho_halo)]
-            kernel.add_equation(halo_densities)
-            kernel.add_equation(halo_momentum)
-            kernel.add_equation(halo_energy)
+        for i in range(1, n_halos+1):
+            # Interpolate temperature into the halos and calculate halo density
+            rho_halo = GridVariable('rho_halo_%d' % i)
+            T_halo = GridVariable('T%d' % i)
+            kernel.add_equation([OpenSBLIEq(T_halo, (i+1)*Twall - i*T_above)])
+            halo_densities += [OpenSBLIEq(rho_halo, pw*gamma*Minf*Minf/T_halo)]
+            # Set momentum in the halos, reverse direction
+            velocities = []
+            for index, momentum_comp in enumerate(NS.momentum()):
+                velocity = GridVariable('u%d%d' % (index, i))
+                halo_momentum += [OpenSBLIEq(increment_dataset(momentum_comp, self.direction, from_side_factor*i), -rho_halo*velocity)]
+                velocities += [velocity**2]
+            # Set energy in halos based on wall pressure, interpolated temperature and the calculated density
+            energy_rhs = pw/(gamma-1) + Rational(1, 2)*rho_halo*(sum(velocities))
+            halo_energy += [OpenSBLIEq(increment_dataset(NS.total_energy(), self.direction, from_side_factor*i), energy_rhs)]
+            halo_densities += [OpenSBLIEq(increment_dataset(NS.density(), self.direction, from_side_factor*i), rho_halo)]
+        kernel.add_equation(halo_densities)
+        kernel.add_equation(halo_momentum)
+        kernel.add_equation(halo_energy)
         kernel.update_block_datasets(block)
         return kernel
