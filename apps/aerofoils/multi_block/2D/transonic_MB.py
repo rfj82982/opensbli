1ab788c3a17fdfda9ff12cc26c79d7d4f85ebd84
David Lusher <lusher.david@jaxa.jp>
2023-08-22 15:47:03 +0900

    Updated 2D airfoil problem

--- apps/aerofoils/multi_block/2D/transonic_MB.py
+++ apps/aerofoils/multi_block/2D/transonic_MB.py
@@ -11,7 +11,7 @@ import itertools
 simulation_parameters = {
 # Substitute simulation parameter values
 'gama'      :   '1.4',
-'Minf'      :   '0.2',
+'Minf'      :   '0.6',
 'Pr'        :   '0.71',
 'Re'        :   '1.0e4',
 'dt'        :   '5.0e-5',
@@ -229,8 +229,44 @@ mb_bcs[2] = block2_bc
 # Set the multi block boundary conditions
 multi_block.set_block_boundaries(mb_bcs)
 
+
+# Add post-processing of vorticity/dilatation fields using new code features
+# Velocity in 2D
+vel = symbols("u0:%d"%ndim,  **{'cls':DataObject})
+# Vorticity-z
+wx, wy, wz = symbols("wx wy wz",  **{'cls':GridVariable})
+# coordinates
+coord = symbols("x0:%d"%ndim,  **{'cls':CoordinateObject})
+# Matrix of derivatives
+der_matrix = Matrix(ndim,ndim,[CentralDerivative(u,x) for u in vel for x in coord])
+post = UserDefinedEquations()
+post.kernel_merge = True
+post.algorithm_place = InTheSimulation(frequency=100)
+post.computation_name = 'Airfoil post-processing'
+post.order = 10000000 # appear at the end of the kernels at the end of the time-loop
+# # # X vorticity
+# vortx = der_matrix[2,1] - der_matrix[1,2]
+# # vortx = metriceq.apply_transformation(vortx)
+# vortx = Eq(wx, vortx)
+# post.add_equations(vortx)
+# # Y vorticity
+# vorty = der_matrix[0,2] - der_matrix[2,0]
+# # vorty = metriceq.apply_transformation(vorty)
+# vorty = Eq(wy, vorty)
+# post.add_equations(vorty)
+# Z vorticity
+vortz = der_matrix[1,0] - der_matrix[0,1]
+vortz = metriceq.apply_transformation(vortz)
+vortz = Eq(DataObject('omega_z'), vortz)
+post.add_equations(vortz)
+# # Dilatation
+divV = symbols("divV", **{'cls':DataObject})
+dil = Eq(divV, der_matrix[0,0] + der_matrix[1,1])
+dil = metriceq.apply_transformation(dil)
+post.add_equations(dil)
+
 # Set the equations on the blocks
-multi_block.set_equations([simulation_eq, constituent, metriceq])
+multi_block.set_equations([simulation_eq, constituent, metriceq, post])
 
 # Add statsistics gathering
 stats = False
@@ -270,13 +306,20 @@ for no, block in enumerate(multi_block.blocks):
         filters[no] += [BinomialFilter(block, order=6, sigma=0.8, grid_condition=grid_condition).equation_classes]
         grid_condition = j >= 475
         filters[no] += [BinomialFilter(block, order=6, sigma=0.8, grid_condition=grid_condition).equation_classes]
+
+
+# Add SFD filtering
+for no, block in enumerate(multi_block.blocks):
+    filters[no] += [SFD(block, chifilt=0.1, omegafilt=1.0/0.75).equation_classes]
+
+# Set the filters on the block
 multi_block.set_filters(filters)
 
 # HDF5 input/output
 kwargs = {'iotype': "Write"}
 q_hdf5 = iohdf5(save_every=1000, **kwargs)
 q_hdf5.add_arrays(simulation_eq.time_advance_arrays)
-q_hdf5.add_arrays([DataObject('kappa')])
+q_hdf5.add_arrays([DataObject('kappa'), DataObject('divV'), DataObject('omega_z'), DataObject('rho_SFD'), DataObject('rhou0_SFD'), DataObject('rhou1_SFD'), DataObject('rhoE_SFD')])
 # Read in the grid file
 kwargs = {'iotype': "Read"}
 x,y = symbols("x0, x1", **{'cls':DataObject})
@@ -317,8 +360,13 @@ for block in multi_block.blocks:
             if eq.full_swap:
                 eq.Kernels += filter_swaps
 
+arrays = ['u1', 'u1', 'u1', 'u1', 'u1', 'u1', 'u1']
+arrays = [block.location_dataset('%s' % dset) for dset in arrays]
+indices = [(25, 1), (50, 1), (150, 1), (250, 1), (350, 1), (450, 1), (550, 1)]
+SM = SimulationMonitor(arrays, indices, multi_block.get_block(2), print_frequency=250, fp_precision=12, output_file='output.log')
+
 # Create the OPS C code
-alg = TraditionalAlgorithmRKMB(multi_block)
+alg = TraditionalAlgorithmRKMB(multi_block, simulation_monitor=SM)
 OPSC(alg, OPS_diagnostics=1)
 # NaN check and iteration counter
 print_iteration_ops(NaN_check='rho', every=100, nblocks=nblocks)
