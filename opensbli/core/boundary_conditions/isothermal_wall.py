e582a9880b4c035ac73d8b187c92e0684adc4ec4
David Lusher <lusher.david@jaxa.jp>
2023-09-07 10:32:10 +0900

    Alternative isothermal wall for channel case

--- opensbli/core/boundary_conditions/isothermal_wall.py
+++ opensbli/core/boundary_conditions/isothermal_wall.py
@@ -9,6 +9,54 @@ from opensbli.core.grid import GridVariable
 from opensbli.schemes.spatial.weno import ShockCapturing
 
 
+class IsothermalWall_ZeroPressureGradBC(ModifyCentralDerivative, BoundaryConditionBase, WallBC):
+    """ Navier-Stokes specific boundary condition. Applies a no-slip viscous wall condition,
+    velocity components are zero on the wall. Temperature is fixed with a prescribed wall temperature,
+    given as the rhoE equation passed to this BC.
+
+    :arg int direction: Spatial direction to apply boundary condition to.
+    :arg int side: Side 0 or 1 to apply the boundary condition for a given direction.
+    :arg Eq equations: Equation for conservative variable rhoE to set on the wall with constant temperature.
+    :arg object scheme: Boundary scheme if required, defaults to Carpenter boundary treatment.
+    :arg bool plane: True/False: Apply boundary condition to full range/split range only."""
+
+    def __init__(self, direction, side, equations, scheme=None, corners=True, plane=True, multi_block=False):
+        BoundaryConditionBase.__init__(self, direction, side, plane)
+        self.bc_name = 'IsothermalWall_ZeroPressureGrad'
+        self.corners = corners
+        self.equations = equations
+        self.multi_block = multi_block
+        if not scheme:
+            self.modification_scheme = Carpenter()
+        else:
+            self.modification_scheme = scheme
+        return
+
+    def apply(self, arrays, block):
+        halos, kernel = self.generate_boundary_kernel(block, self.bc_name, corners=self.corners)
+        n_halos = abs(halos[self.direction][self.side])
+        # Using Navier Stokes physics object, create conservative variables
+        NS = NSphysics(block)
+        from_side_factor, to_side_factor = self.set_side_factor()
+        wall_eqns = []
+        # Update halos if a shock capturing scheme is being used.
+        # if any(isinstance(sc, ShockCapturing) for sc in block.discretisation_schemes.values()) and self.shock:
+        pw, gamma, Minf, Twall = GridVariable('P_above'), NS.specific_heat_ratio(), NS.mach_number(), ConstantObject('Twall')
+        from opensbli.core.kernel import ConstantsToDeclare as CTD
+        CTD.add_constant(Twall)
+        # Wall pressure
+        wall_eqns += [OpenSBLIEq(pw, increment_dataset(NS.pressure(relation=True, conservative=True), self.direction, to_side_factor*1))]
+        # Set wall conditions, momentum zero
+        wall_eqns += [OpenSBLIEq(x, Float(S.Zero)) for x in NS.momentum()]
+        # Set density and energy on the wall
+        wall_eqns += [OpenSBLIEq(NS.density(), pw*Twall*gamma*Minf*Minf)] # p_inf = 1/(gamma*Minf^2)
+        wall_eqns += [OpenSBLIEq(NS.total_energy(), pw / (gamma - 1))]
+        kernel.add_equation(wall_eqns)
+        # Direct copy indices over the wall
+        kernel.update_block_datasets(block)
+        print("Using the zero pressure gradient version of the isothermal wall. Please set the residual arrays to zero on the wall plane for this BC.")
+        return kernel
+
 class IsothermalWallBC(ModifyCentralDerivative, BoundaryConditionBase, WallBC):
     """ Navier-Stokes specific boundary condition. Applies a no-slip viscous wall condition,
     velocity components are zero on the wall. Temperature is fixed with a prescribed wall temperature,
